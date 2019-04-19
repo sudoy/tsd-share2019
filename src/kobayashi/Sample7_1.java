e67cb109411cce6bfe2a576a1984e7d80f3cfb5a
小林 <@TSD-KOBAYASHI>
2019-04-19 09:00:55 +0900

    Sample7_1,

---
+++ src/kobayashi/Sample7_1.java
@@ -0,0 +1,18 @@
+package kobayashi;
+
+public class Sample7_1 {
+
+	public static void main(String[] args) {
+		int x = 15;
+		int y = 0;
+		try {
+			System.out.println("x / y: " + x / y);
+			System.out.println("x * y: " + x * y);
+		}catch(ArithmeticException ae){
+			System.out.println("発生した例外:" + ae);
+		}
+		System.out.println("x - y:"  + (x - y));
+
+	}
+
+}
