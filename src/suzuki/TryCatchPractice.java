b530b827bb8accfc2c6f6d5e79c0125ba1716acd
鈴木 <@TSD-SUZUKI>
2019-04-19 11:29:02 +0900

    TryCatchPractice

--- src/suzuki/TryCatchPractice.java
+++ src/suzuki/TryCatchPractice.java
@@ -6,22 +6,21 @@ public class TryCatchPractice {
 
 		String[] a1 = {"5", "23", "123", "89", "91", "23"};
 		String[] a2 = {"5", "23", "a", "89", "b", "c"};
-		String[] a3 = {"5", "23", "89"};
 
 		int[] x = makeArray4(a2);
 		for(int n : x) {
-			System.out.print(n + ",");
+			System.out.print(n + " ");
 		}
 
-
 	}
+
 
 	public static int[] makeArray1(String[] a) {
 
 		int[] b = new int[a.length];
 		for(int i = 0; i < a.length; i++) {
-			int b2 = Integer.parseInt(a[i]);
-			b[i] = b2;
+			int x = Integer.parseInt(a[i]);
+			b[i] = x;
 		}
 		return b;
 	}
@@ -30,11 +29,10 @@ public class TryCatchPractice {
 
 		int[] b = new int[a.length];
 		for(int i = 0; i < a.length; i++) {
-			int b2;
 			try {
-				 b2 = Integer.parseInt(a[i]);
-				 b[i] = b2;
-			}catch(Throwable e){
+				int x = Integer.parseInt(a[i]);
+				b[i] = x;
+			}catch(RuntimeException e) {
 
 			}
 		}
@@ -44,13 +42,13 @@ public class TryCatchPractice {
 	public static int[] makeArray3(String[] a) {
 
 		int[] b = new int[a.length];
-		int b2;
+		int x;
 		try {
 			for(int i = 0; i < a.length; i++) {
-				b2 = Integer.parseInt(a[i]);
-				b[i] = b2;
+				x = Integer.parseInt(a[i]);
+				b[i] = x;
 			}
-		}catch(Throwable e) {
+		}catch(RuntimeException e) {
 
 		}
 		return b;
@@ -59,20 +57,18 @@ public class TryCatchPractice {
 	public static int[] makeArray4(String[] a) {
 
 		int[] b = new int[a.length];
-		int b2;
 		try {
 			for(int i = 0; i < a.length; i++) {
-				b2 = Integer.parseInt(a[i]);
-				b[i] = b2;
+				int x = Integer.parseInt(a[i]);
+				b[i] = x;
 			}
-		}catch(Throwable e) {
+		}catch(RuntimeException e) {
 			for(int i = 0; i < a.length; i++) {
 				b[i] = 0;
 			}
 		}
 		return b;
 	}
-
 }
 
 
