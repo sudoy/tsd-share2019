70604117d03777d3512c5f3f2a40a36b9570b656
山本 <@TSD-YAMAMOTO>
2019-04-19 11:22:19 +0900

    makeArray2の作成

--- src/yamamoto/TryCatchPractice.java
+++ src/yamamoto/TryCatchPractice.java
@@ -4,8 +4,10 @@ public class TryCatchPractice {
 
 	public static void main(String[] args) {
 		String[] a1 = {"5", "23", "123", "89", "91", "23"};
+		String[] a2 = {"5", "23", "a", "89", "b", "c"};
 		int[] n1 = makeArray1(a1);
-		for(int x:n1) {
+		int[] n2 = makeArray2(a2);
+		for(int x:n2) {
 			System.out.println(x);
 		}
 	}
@@ -14,6 +16,18 @@ public class TryCatchPractice {
 		int[] ans = new int[a.length];
 		for(int i = 0; i < ans.length; i++) {
 			ans[i] = Integer.valueOf(a[i]);
+		}
+		return ans;
+	}
+
+	public static int[] makeArray2(String[] a) {
+		int[] ans = new int[a.length];
+		for(int i = 0; i < ans.length; i++) {
+			try {
+				ans[i] = Integer.valueOf(a[i]);
+			}catch (NumberFormatException e) {
+			}
+
 		}
 		return ans;
 	}
