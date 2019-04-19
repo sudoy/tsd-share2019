d817fe4772e9a7e04cb7ccbf0f866506194b7461
山本 <@TSD-YAMAMOTO>
2019-04-19 11:19:09 +0900

    TryCatchPracticeの再作成

--- src/yamamoto/TryCatchPractice.java
+++ src/yamamoto/TryCatchPractice.java
@@ -5,60 +5,16 @@ public class TryCatchPractice {
 	public static void main(String[] args) {
 		String[] a1 = {"5", "23", "123", "89", "91", "23"};
 		int[] n1 = makeArray1(a1);
-		String[] a2 = {"5", "23", "a", "89", "b", "c"};
-		int[] n2 = makeArray2(a2);
-		int[] n3 = makeArray3(a2);
-		int[] n4 = makeArray4(a2);
-
- 		for(int i = 0;i < n4.length; i++) {
-			System.out.println(n4[i]);
+		for(int x:n1) {
+			System.out.println(x);
 		}
-
 	}
 
 	public static int[] makeArray1(String[] a) {
 		int[] ans = new int[a.length];
- 		for(int i = 0; i < a.length; i++) {
+		for(int i = 0; i < ans.length; i++) {
 			ans[i] = Integer.valueOf(a[i]);
 		}
- 		return ans;
+		return ans;
 	}
-
-	public static int[] makeArray2(String[] a) {
-		int[] ans = new int[a.length];
- 		for(int i = 0; i < a.length; i++) {
- 			try {
- 				ans[i] = Integer.valueOf(a[i]);
- 			}catch (NumberFormatException e) {
-			}
-
-		}
- 		return ans;
-	}
-
-	public static int[] makeArray3(String[] a) {
-		int[] ans = new int[a.length];
-		try {
-	 		for(int i = 0; i < a.length; i++) {
-	 			ans[i] = Integer.valueOf(a[i]);
-			}
-		}catch (NumberFormatException e) {
-		}
- 		return ans;
-	}
-
-	public static int[] makeArray4(String[] a) {
-		int[] ans = new int[a.length];
-		try {
-	 		for(int i = 0; i < a.length; i++) {
-	 			ans[i] = Integer.valueOf(a[i]);
-			}
-		}catch (NumberFormatException e) {
-			ans = new int[a.length];
-		}
- 		return ans;
-	}
-
-
-
 }
