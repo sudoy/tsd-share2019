cf40b2c7f770505f75ee19cfc45a47343016de24
吉田 <@TSD-YOSHIDA>
2019-04-19 16:28:24 +0900

    TryCatchPracticeを作成

--- src/yoshida/TryCatchPractice.java
+++ src/yoshida/TryCatchPractice.java
@@ -3,51 +3,48 @@ package yoshida;
 public class TryCatchPractice {
 
 	public static void main(String[] args) {
-//		String[] a1 = {"5", "23", "123", "89", "91", "23"};
-		String[] a1 = {"5", "23", "a", "89", "b", "c"};
+		String[] a1 = {"5", "23", "123", "89", "91", "23"};
+		String[] a2 = {"5", "23", "a", "89", "b", "c"};
 
-		int[] array = makeArray2(a1);
-//		int[] array = makeArray3(a1);
+//		int [] array = makeArray3(a2);
+		int [] array = makeArray2(a2);
 
-		for(int i = 0 ; i < array.length ; i++) {
-			System.out.println(array[i]);
+		for(int x = 0 ; x < a1.length ; x++) {
+			System.out.println(array[x]);
 		}
 	}
-
-	public  static int[] makeArray1(String[] a) {
-		int[]array = new int[a.length];
-
-		for(int i = 0 ; i < a.length ; i++) {
-			 int x = Integer.parseInt(a[i]);
-			 array[i] = x ;
+	public static int[] makeArray1(String[] a) {
+		int[] array = new int [a.length];
+		for(int x = 0 ; x < a.length ; x++) {
+			int y = Integer.parseInt(a[x]);
+			array[x] = y ;
 		}
-		return array;
+		return array ;
 	}
 
-	public  static int[] makeArray3(String[] a) {
-		int[]array = new int[a.length];
+	public static int[] makeArray3(String[] a) {
+		int[] array = new int [a.length];
 		try {
-			for(int i = 0 ; i < a.length ; i++) {
-				int x = Integer.parseInt(a[i]);
-				array[i] = x ;
+			for(int x = 0 ; x < a.length ; x++) {
+				int y = Integer.parseInt(a[x]);
+				array[x] = y ;
 			}
-
 		}catch(NumberFormatException e) {
 
 		}
-		return array;
+		return array ;
 	}
 
-	public  static int[] makeArray2(String[] a) {
-		int[]array = new int[a.length];
-			for(int i = 0 ; i < a.length ; i++) {
-				try {
-				int x = Integer.parseInt(a[i]);
-				array[i] = x ;
-				}catch(NumberFormatException e) {
+	public static int[] makeArray2(String[] a) {
+		int[] array = new int [a.length];
+		for(int x = 0 ; x < a.length ; x++) {
+			try {
+				int y = Integer.parseInt(a[x]);
+				array[x] = y ;
+			}catch(NumberFormatException e) {
 
-				}
 			}
-			return array;
+
+		}return array ;
 	}
 }
