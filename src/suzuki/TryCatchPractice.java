f57250dafe0c1aca8b996607636be5442d9fceff
鈴木 <@TSD-SUZUKI>
2019-04-19 11:01:24 +0900

    TryCatchPractice

--- src/suzuki/TryCatchPractice.java
+++ src/suzuki/TryCatchPractice.java
@@ -6,8 +6,9 @@ public class TryCatchPractice {
 
 		String[] a1 = {"5", "23", "123", "89", "91", "23"};
 		String[] a2 = {"5", "23", "a", "89", "b", "c"};
+		String[] a3 = {"5", "23", "89"};
 
-		int[] x = makeArray3(a2);
+		int[] x = makeArray4(a2);
 		for(int n : x) {
 			System.out.print(n + ",");
 		}
@@ -51,6 +52,23 @@ public class TryCatchPractice {
 			}
 		}catch(Throwable e) {
 
+		}
+		return b;
+	}
+
+	public static int[] makeArray4(String[] a) {
+
+		int[] b = new int[a.length];
+		int b2;
+		try {
+			for(int i = 0; i < a.length; i++) {
+				b2 = Integer.parseInt(a[i]);
+				b[i] = b2;
+			}
+		}catch(Throwable e) {
+			for(int i = 0; i < a.length; i++) {
+				b[i] = 0;
+			}
 		}
 		return b;
 	}
