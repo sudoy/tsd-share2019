2a0e472724cca96a98d4d66f619a93d5b21162f3
山本 <@TSD-YAMAMOTO>
2019-04-22 14:14:25 +0900

    LambdaExpression2の作成

---
+++ src/yamamoto/LambdaExpression2.java
@@ -0,0 +1,35 @@
+package yamamoto;
+
+public class LambdaExpression2 {
+
+	public static void main(String[] args) {
+		System.out.println(new SummaryImpl().exec(10, 5));
+		Summary s1 = new Summary() {
+			@Override
+			public int exec(int a, int b) {
+				return a + b;
+			}
+		};
+		Summary s2 = (int a, int b) ->{
+			return a + b;
+		};
+		Summary s3 = (int a, int b) -> a + b;
+		System.out.println(s1.exec(10, 5));
+		System.out.println(s2.exec(10, 5));
+		System.out.println(s3.exec(10, 5));
+//		System.out.println(new Summary() {@Override public int exec(int a, int b) {return a + b;}}.exec(11, 3));
+	}
+
+}
+
+interface Summary{
+	public int exec(int a, int b);
+}
+
+class SummaryImpl implements Summary{
+	@Override
+	public int exec(int a, int b) {
+		return a + b;
+	}
+
+}
