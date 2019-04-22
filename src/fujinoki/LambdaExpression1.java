166435d858ddf0de8ccfdb9621fd337844912e0d
藤ノ木 <@TSD-FUJINOKI>
2019-04-22 14:49:12 +0900

    lambdaexpression1

--- src/fujinoki/LambdaExpression1.java
+++ src/fujinoki/LambdaExpression1.java
@@ -23,6 +23,8 @@ public class LambdaExpression1 {
 			}
 		};
 		System.out.println(p2.test(1));
+		System.out.println(p2.test(-1));
+
 
 		//ラムダ式（省略なし）
 		Predicate<Integer>p3 = (Integer t) ->{
@@ -32,12 +34,16 @@ public class LambdaExpression1 {
 				return true;
 			}
 		};
-		
+		System.out.println(p3.test(1));
+		System.out.println(p3.test(-1));
+
+
 		//ラムダ式（省略あり）
 		Predicate<Integer> p4 = t -> 0 < t;
 		System.out.println(p4.test(1));
+		System.out.println(p4.test(-1));
 	}
-	
+
 }
 
 class Predicateimpl implements Predicate<Integer>{
