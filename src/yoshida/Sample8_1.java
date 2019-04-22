1f6dc12e1f0c0c8e809ea2b528c9bb1aeebdf58f
吉田 <@TSD-YOSHIDA>
2019-04-22 10:02:47 +0900

    Sample8_1作成

---
+++ src/yoshida/Sample8_1.java
@@ -0,0 +1,30 @@
+package yoshida;
+
+public class Sample8_1 {
+
+	public static void main(String[] args) {
+		String ext = "From external class" ;
+		MyTopLevel mt = new MyTopLevel();
+
+		MyTopLevel.MyInner inner = mt.new MyInner();
+
+		inner.accessInner(ext);
+	}
+
+}
+
+class MyTopLevel {
+	private String top = "From Top level class" ;
+	MyInner minn = new MyInner() ;
+
+	public void createNested() {
+
+		minn.accessInner(top);
+	}
+
+	class MyInner{
+		public void accessInner(String st) {
+			System.out.println(st);
+		}
+	}
+}
