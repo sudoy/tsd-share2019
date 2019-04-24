8abb90eb7f4d8d68bb53db2010cc42d8e890ebcc
藤ノ木 <@TSD-FUJINOKI>
2019-04-24 09:14:43 +0900

    422

--- src/fujinoki/LocalDate.java
+++ src/fujinoki/LocalDate.java
@@ -1,26 +1,19 @@
 package fujinoki;
 
-public class LocalDate {
+import java.time.LocalDate;
+
+public class Mondai1 {
 
 	public static void main(String[] args) {
 		
 		LocalDate now = LocalDate.now();
 		
-		LocalDate date = LocalDate.of(now,now,1);
-		System.out.println(date);
+		
+		LocalDate firstdate = 
 		
 	
 
 
-	}
 
-	private static LocalDate now() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	private static LocalDate of(LocalDate now, LocalDate now2, int i) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
 	}
 }
