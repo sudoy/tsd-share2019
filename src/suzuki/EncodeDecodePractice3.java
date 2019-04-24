e2074f9e7770a755500b6993d1bde3aaeed77844
鈴木 <@TSD-SUZUKI>
2019-04-24 10:50:15 +0900

    EncodeDecodePractice3

---
+++ src/suzuki/EncodeDecodePractice3.java
@@ -0,0 +1,35 @@
+package suzuki;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class EncodeDecodePractice3 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.print("ファイル名：");
+		String name = scn.nextLine();
+		System.out.println();
+		scn.close();
+
+		File file = new File("C:\\output\\"+ name);
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(file));
+
+			String line = in.readLine();
+			System.out.println("…\n読み込み完了！");
+			System.out.println("-------\n" + line + "\n-------");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+
+		}
+	}
+
+}
