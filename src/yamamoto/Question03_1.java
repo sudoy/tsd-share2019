08f2a1f57fc905f8b917274fadc386f598761661
山本 <@TSD-YAMAMOTO>
2019-05-09 16:02:55 +0900

    Kadaiの追加

--- src/yamamoto/Question03_1.java
+++ src/yamamoto/Question03_1.java
@@ -5,7 +5,28 @@ import java.util.ArrayList;
 public class Question03_1 {
 
 	public static void main(String[] args) {
-		
+		// SyainListインスタンス生成
+        SyainList sl = new SyainList();
+
+        // 社員生成およびSyainListインスタンスへ追加（パターン１）
+        sl.addSyain("1", "山田", "技術部");
+        sl.addSyain("2", "佐藤", "営業部");
+
+        // 社員生成およびSyainListインスタンスへ追加（パターン２）
+        sl.add(new Syain("3", "井上", "営業部"));
+
+        // 社員生成およびSyainListインスタンスへ追加（パターン３）
+        Syain s2 = new Syain();
+        s2.setId("4");
+        s2.setName("田口");
+        s2.setBusyo("総務部");
+        sl.add(s2);
+
+        // 社員一覧出力
+        for(Syain s : sl) {
+            System.out.println(s);
+        }
+        System.out.println(String.format("\n計 %d 名", sl.size()));
 	}
 
 }
@@ -14,12 +35,16 @@ class Syain{
 	private String	id;
 	private String	name;
 	private String	busyo;
+
 	public	Syain() {
-		
+
 	}
 	public	Syain(String id, String name, String busyo) {
-		
+		this.id = id;
+		this.name = name;
+		this.busyo = busyo;
 	}
+
 	public String getId() {
 		return id;
 	}
@@ -38,9 +63,13 @@ class Syain{
 	public void setBusyo(String busyo) {
 		this.busyo = busyo;
 	}
+
+	public String toString() {
+		return id + ", " + name + ", " + busyo;
+	}
 }
 class SyainList extends ArrayList<Syain>{
 	public void addSyain(String id, String name, String busyo) {
-		
+		add(new Syain(id, name, busyo));
 	}
 }
