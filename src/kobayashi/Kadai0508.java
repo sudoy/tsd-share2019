1b030bbe89f22907668ecc124e9016ae9f901e9c
小林 <@TSD-KOBAYASHI>
2019-05-10 17:31:52 +0900

    kadai0508

--- src/kobayashi/Kadai0508.java
+++ src/kobayashi/Kadai0508.java
@@ -1,6 +1,5 @@
 package kobayashi;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,12 +8,11 @@ public class Kadai0508 {
 	public static void main(String[] args) {
 
 		System.out.println("整数(1~65535)を入力して下さい。");
-
+		Scanner scn = new Scanner(System.in);
 		int a = 0;
 
 		for (;;) {
 			try {
-				Scanner scn = new Scanner(System.in);
 				System.out.print("整数を入力 :");
 
 				a = scn.nextInt();
@@ -24,23 +22,46 @@ public class Kadai0508 {
 				} else {
 					System.out.println("不正な値です。");
 				}
-				scn.close();
 			} catch (InputMismatchException ie) {
 				System.out.println("不正な値です。");
 			}
 		}
-		conversion(a,2);
-		conversion(a,16);
+		scn.close();
+
+		conversion(a, 2);
+		conversion(a, 16);
 	}
 
-	public static void conversion(int x,int y) {
-		StringBuilder sb = new StringBuilder();
-			int x = num;
-		while (a != 0) {
-			x / num;
-		}
+	public static void conversion(int input, int bnum) {
 
+		StringBuilder sb = new StringBuilder();
+		int num = input;
+
+		while (num != 0) {
+			String rem = String.valueOf(num % bnum);
+			switch (rem) {
+			case "10":
+				rem = "A";
+				break;
+			case "11":
+				rem = "B";
+				break;
+			case "12":
+				rem = "C";
+				break;
+			case "13":
+				rem = "D";
+				break;
+			case "14":
+				rem = "E";
+				break;
+			case "15":
+				rem = "F";
+				break;
+			}
+			sb.append(rem);
+			num /= bnum;
 		}
-	//for (int i = array.size() - 1; i >= 0; i--) {
-	//System.out.print(array.get(i));
+		System.out.println("10進数の値 " +  input  + " は " + bnum  + " 進数の " + sb.reverse() + " です。 ");
+	}
 }
