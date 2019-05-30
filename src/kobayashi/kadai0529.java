1808b13b164f0da07596a817d96825d3323c5d5c
小林 <@TSD-KOBAYASHI>
2019-05-30 17:30:00 +0900

    修正

--- src/kobayashi/kadai0529.java
+++ src/kobayashi/kadai0529.java
@@ -1,5 +1,13 @@
 package kobayashi;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -8,32 +16,80 @@ public class kadai0529 {
 
 	public static void main(String[] args) {
 
-		Calendar cal = Calendar.getInstance();//カレンダークラス
+		Calendar cal = Calendar.getInstance();
 
-		System.out.print("カレンダーを出力する年月を入力してください。");
-
+		System.out.print("カレンダーを出力する年月を入力してください。");//年月の入力
 		Scanner scn = new Scanner(System.in);
+		String y;
+		int m = 0;
 
 		for (;;) {
 			try {
-				String y = scn.nextLine();
-				String m = scn.next();
+				 y = scn.next();
+				 m = 0;
 
 				if (y.length() == 4) {
-//					&& m.matches("[0-12]") ) {
-//				}
+					m = scn.nextInt();
+				}
+				if (1 <= m && m <= 12) {
 					break;
 				} else {
-						System.out.println("入力内容が不正です。もう一度入力してください。");
+					System.out.println("入力内容が不正です。もう一度入力してください。");
 				}
 			} catch (InputMismatchException ie) {
 				System.out.println("入力内容が不正です。もう一度入力してください。");
+				scn.nextLine();
 			}
 		}
 		scn.close();
 
 		System.out.println("ファイルに出力しました。");
 
+		LocalDate date = LocalDate.of(Integer.valueOf(y), m,1);//今月1日(変数として使うと便利)
+		date.getDayOfWeek();
+		date.plusMonths(1).minusDays(1);//今月の最終日
+
+
+		File inputFile = new File("C:\\HTMLCSS課題\\kadai0529.html");
+		File outputFile = new File("C:\\HTMLCSS課題\\kadai0529.html");
+
+		BufferedReader in = null;
+		BufferedWriter out = null;
+
+
+		try {
+			in = new BufferedReader(new FileReader(inputFile));
+			out = new BufferedWriter(new FileWriter(outputFile));
+
+			String line;
+			while((line = in.readLine()) != null){
+				out.write(line);
+				out.newLine();
+				//out.println(line); //上2行の代わり
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}finally{
+			try {
+				if (in != null) {
+				in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if(out != null) {
+				out.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+
 
 }
