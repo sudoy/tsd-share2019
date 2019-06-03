d4a3d43241ec762a27f072874bb21646c647dc6d
小林 <@TSD-KOBAYASHI>
2019-06-03 10:46:47 +0900

    修正

--- src/jp/co/tsd/training2019/kobayashi/Question02.java
+++ src/jp/co/tsd/training2019/kobayashi/Question02.java
@@ -1,40 +1,41 @@
 package jp.co.tsd.training2019.kobayashi;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Question02 {
 
 	public static void main(String[] args) {
 
-		for (;;) {
-			try {
-				Scanner scn = new Scanner(System.in);
+		Scanner scn = new Scanner(System.in);
 
-				System.out.println("開始する数値を入力してください。");
-				int start = scn.nextInt();
-				System.out.println("終了する数値を入力してください。");
-				int finish = scn.nextInt();
+		try {
+			System.out.print("開始する数値を入力してください。-->");
+			String strs = scn.next();
+			System.out.print("終了する数値を入力してください。-->");
+			String strf = scn.next();
 
+			scn.close();
 
+			int start = Integer.parseInt(strs);
+			int finish = Integer.parseInt(strf);
 
-				int x = finish - start;
-				int total = 0;
+			int total = 0;
 
-					for (int i = start; start <= finish; i += 2) {
-						total = total + i;
+			for (int i = start; i <= finish; i++) {
 
-					}
+				if (total + i > 100) {
+					System.out.println("計算結果が100を超えたため、処理を中断しました");
+					System.out.println("超える前の総和は" + total + "になります。");
+					return;
 
-
-
-
-				scn.close();
-
-			} catch (InputMismatchException e) {
-				System.out.println("整数値を入力してください。");
+				} else if (i % 2 == 0) {
+					total += i;
+				}
 			}
+			System.out.println(start + "から" + finish + "までの総和は" + total + "になります");
 
+		} catch (NumberFormatException e) {
+			System.out.println("整数値を入力してください。");
 		}
 
 	}
